d3884d41f01aacee920fe97640c6d5442e26a9c9
“PoisebTechHub” <sadikadams23@gmail.com>
2024-06-22 14:27:52 +0000

    done

--- soac_user/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StackusersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'soac_user'
-
-    def ready(self):
-        import soac_user.signals
