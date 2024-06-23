ca1f168690f9a2639b3abad0c93d6180720cc13c
“PoisebTechHub” <sadikadams23@gmail.com>
2024-06-23 13:07:11 +0000

    done

--- soac_users/forms.py
+++ soac_users/forms.py
@@ -44,4 +44,7 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'city', 'image']
