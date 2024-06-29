bafce013413f5cf19940a08b9d7312a9b8358a3c
Vuyisile Tlotliso Lehola <73833164+vtl-28@users.noreply.github.com>
2024-06-29 06:05:38 +0200

    add migrations step

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python3.8 manage.py collectstatic --noinput
+# Build the project
+echo "Building the project..."
+python3.8 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.8 manage.py makemigrations --noinput
+python3.8 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.8 manage.py collectstatic --noinput --clear
