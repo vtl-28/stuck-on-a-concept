3c2b44b3934a2dabc75483cc2f3b15b0fb6eda66
Vuyisile Tlotliso Lehola <73833164+vtl-28@users.noreply.github.com>
2024-06-30 02:11:20 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
-pip install -r requirements.txt
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
